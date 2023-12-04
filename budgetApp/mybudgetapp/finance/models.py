5a18826da59f14a180d804ab82087bfb19e3f66f
marta <martambanasiewicz@gmail.com>
2023-12-04 21:14:13 +0100

    1

--- budgetApp/mybudgetapp/finance/models.py
+++ budgetApp/mybudgetapp/finance/models.py
@@ -1,27 +1,15 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-from django.contrib.auth.models import User
-
-class BankAccount(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    bank_name = models.CharField(max_length=100)
-    account_type = models.CharField(max_length=50)
-    account_number = models.CharField(max_length=50)
-
-class Transaction(models.Model):
-    account = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
+class BankBalance(models.Model):
     date = models.DateField()
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Expense(models.Model):
+    date = models.DateField()
+    category = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-class Budget(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    budget_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    start_date = models.DateField()
-    end_date = models.DateField()
+class Investment(models.Model):
+    date = models.DateField()
+    type = models.CharField(max_length=100)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
